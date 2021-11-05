3110315731b6e31c45a8fc7d10d7a919f7680af2
Weichen Xu <weichen.xu@databricks.com>
2021-11-05 17:37:47 +0800

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

---
+++ mlflow/store/db_migrations/versions/bc4103e391c6_update_timestamp_field_default_value.py
@@ -0,0 +1,24 @@
+"""update timestamp field default value
+
+Revision ID: bc4103e391c6
+Revises: c48cb773bb87
+Create Date: 2021-11-05 17:36:30.433343
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc4103e391c6'
+down_revision = 'c48cb773bb87'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
