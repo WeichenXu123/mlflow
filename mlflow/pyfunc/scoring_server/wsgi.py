91dc917824c11addc17865b3b44df89542b8f0dd
Weichen Xu <weichen.xu@databricks.com>
2024-06-27 11:22:01 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/pyfunc/scoring_server/wsgi.py
+++ mlflow/pyfunc/scoring_server/wsgi.py
@@ -2,4 +2,5 @@ import os
 
 from mlflow.pyfunc import load_model, scoring_server
 
+raise RuntimeError("Test failure 123423")
 app = scoring_server.init(load_model(os.environ[scoring_server._SERVER_MODEL_PATH]))
