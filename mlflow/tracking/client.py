16a705333221e9f3af9c12feb237312a5470120e
Weichen Xu <weichen.xu@databricks.com>
2025-10-21 20:26:14 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- mlflow/tracking/client.py
+++ mlflow/tracking/client.py
@@ -478,7 +478,7 @@ class MlflowClient:
         """
         import traceback
         with open("/tmp/dbg1.out", "a") as f:
-            trace = ''.jion(traceback.format_stack())
+            trace = ''.join(traceback.format_stack())
             f.write(f"DBG: creating run: {run_name}\n{trace}\n================\n")
         return self._tracking_client.create_run(experiment_id, start_time, tags, run_name)
 
